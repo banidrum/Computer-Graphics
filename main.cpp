f645bc61210dfc1cfb82fae92516eb79c064584e
Daniel Brum <dani3544@gmail.com>
2019-09-23 20:31:33 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,102 @@
+/**
+ * Simple demonstration of transformations.
+ */
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+
+
+
+void renderCoordinateAxis()
+{
+	// X axis - green color
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+		// Left side, negative X
+		glVertex2f(-1.0, 0.0);
+		glVertex2f(0.0, 0.0);
+
+		// Right side, positive X
+		glVertex2f(0.0, 0.0);
+		glVertex2f(1.0, 0.0);
+	glEnd();
+
+	// Y axis - blue color
+	glColor3f(0, 0, 1);
+	glBegin(GL_LINES);
+		// Top side, positive Y
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.0, 1.0);
+
+		// Bottom side, negative Y
+		glVertex2f(0.0, 0.0);
+		glVertex2f(0.0, -1.0);
+	glEnd();
+}
+
+void display()
+{
+	// Clear the screen painting it all with the white color
+	glClearColor(1, 1, 1, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// Inform OpenGL we want to make changes to the modelview matrix
+	glMatrixMode(GL_MODELVIEW);
+
+	glLoadIdentity();
+	// Render the X and Y axis to guide ourselves.
+	renderCoordinateAxis();
+
+	// Render a red square
+	glColor3f(1, 0, 0);
+	glRotatef(25.0f, 0.0f, 1.0f, 0.0f);
+	glTranslatef(0.2f, 0.2f, 0);
+	glRectf(-0.1f, 0.1f, 0.1f, -0.1f);
+
+	glLoadIdentity();
+
+	// Render a blue square
+	// glColor3f(0, 0, 1);
+	// glTranslatef(0.2f, 0.2f, 0);
+	// glRotatef(45.0f, 0, 0, 1.0f);
+	// glRectf(-0.1f, 0.1f, 0.1f, -0.1f);
+
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	if (key == 27) {
+		// ESC key
+		exit(0);
+	}
+
+	if(key == '1') {
+		glRotatef(50.0f, 1.0f, 0.0f, 0.0f);
+		// glLoadIdentity();
+	}
+
+	if(key == '2') {
+		glTranslatef(0.4f, 0.4f, 0);
+		// glLoadIdentity();
+	}
+
+	if(key == '3') {
+		glScalef(1.0f, 1.0f, 0.0f);
+		// glLoadIdentity();
+	}
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("Transformation - Simple");
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutKeyboardFunc(keyboard);
+
+	glutMainLoop();
+	return 0;
+}
